79798978269df464d03fffc6781f90a9b2732070
gabriel teodoro <gabrielteodoro@pop-os.localdomain>
2023-10-23 14:40:58 -0300

    Components e paginas iniciais

---
+++ src/components/button/button.tsx
@@ -0,0 +1,69 @@
+import imgGearGold from "./../../assets/icons/gearOrange.svg";
+import imgGearWhite from "./../../assets/icons/gearWhite.svg";
+import imgGearPurple from "./../../assets/icons/gearPurple.svg";
+import { useState } from "react";
+
+export default function Button({
+  text,
+  onClick,
+  type,
+  color,
+  img,
+}: {
+  text: string;
+  onClick?: (e?: any) => void;
+  type?: "submit" | "reset" | "button";
+  color: string;
+  img?: boolean;
+}) {
+  const [hover, onHover] = useState(false);
+
+  return (
+    <>
+      <button
+        className={`flex items-center justify-evenly w-full h-10 transition-all duration-300 ease-in-out 
+        ${
+          color === "purple"
+            ? "bg-purpleDark text-gold border-gold hover:bg-gold hover:text-purpleDark"
+            : color === "gold"
+            ? "bg-gold text-purpleDark border-goldDark hover:bg-transparent hover:text-goldDark"
+            : "bg-transparent text-purpleDark border-transparent hover:bg-purpleDark hover:text-white"
+        } rounded-3xl border-2 `}
+        type={type ? type : "button"}
+        onClick={onClick}
+        onMouseEnter={() => {
+          onHover(true);
+        }}
+        onMouseLeave={() => {
+          onHover(false);
+        }}
+      >
+        {text}
+        {img && !hover && (
+          <img
+            src={
+              color === "purple"
+                ? imgGearGold
+                : color === "gold"
+                ? imgGearPurple
+                : imgGearPurple
+            }
+            alt="icon setting"
+          />
+        )}
+        {img && hover && (
+          <img
+            src={
+              color === "purple"
+                ? imgGearPurple
+                : color === "gold"
+                ? imgGearGold
+                : imgGearWhite
+            }
+            alt="icon setting"
+          />
+        )}
+      </button>
+    </>
+  );
+}
---
+++ src/components/employee/employee.tsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+export default function Employee({
+  id,
+  img,
+  name,
+  classname,
+  setShowModalDelete,
+}: {
+  id: number;
+  img: string;
+  name: string;
+  classname?: string;
+  setShowModalDelete: any;
+}) {
+  return (
+    <div
+      className={`flex items-center justify-between w-full p-4 ${classname}`}
+    >
+      <img
+        className="h-24 w-24 rounded-[100%] border-purpleDark border-2"
+        src={img}
+        alt={`img ${name}`}
+      />
+      <h1 className="text-lg">{name}</h1>
+      <h2
+        onClick={() => {
+          setShowModalDelete(id);
+        }}
+        className="w-8 h-8 text-white bg-purpleDark flex items-center justify-center rounded-[100%] text-xl"
+      >
+        X
+      </h2>
+    </div>
+  );
+}
